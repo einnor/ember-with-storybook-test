979102f32ae497b63c704e7aadda388508aa4f8a
Ronnie Nyaga <ronnienyaga@gmail.com>
2020-07-09 14:30:13 +0300

    Added button story

---
+++ stories/button.stories.js
@@ -0,0 +1,18 @@
+import { hbs } from 'ember-cli-htmlbars';
+
+// <Button />
+// @label string
+// @type can be primary, warn, accent
+// @icon is the name if a mui icon
+// @disabled is the name if a mui icon
+// @fab is floating action button
+// @raised boolean
+
+export default {
+  title: 'Button',
+};
+
+export const Default = () =>
+hbs`
+  <Button @label="Button" />
+`;
